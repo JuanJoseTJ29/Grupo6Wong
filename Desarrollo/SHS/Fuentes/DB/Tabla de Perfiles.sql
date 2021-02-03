eb4c6fee8e38daa05a77dc989e6be818964dec6f
Juan Tirado <juan.tirado@unmsm.edu.pe>
2021-02-03 11:22:17 -0500

    Se actualizo las tablas de la base de datos

---
+++ Desarrollo/SHS/Fuentes/DB/Tabla de Perfiles.sql
@@ -0,0 +1,22 @@
+-- --------------------------------------------------------
+-- Host:                         localhost
+-- Versión del servidor:         5.7.24 - MySQL Community Server (GPL)
+-- SO del servidor:              Win64
+-- HeidiSQL Versión:             10.2.0.5599
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Volcando datos para la tabla shs.profiles: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `profiles` DISABLE KEYS */;
+INSERT INTO `profiles` (`id`, `user_id`, `firstname`, `lastname`, `phone_number`, `address`, `profession`, `created_at`, `updated_at`) VALUES
+	(1, 1, NULL, NULL, NULL, NULL, NULL, '2021-02-02 22:32:33', '2021-02-02 22:32:33');
+/*!40000 ALTER TABLE `profiles` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
